48b8a178cacbaac8bdaeed72eab4b57522e34e62
daiki <dik.tkmr.1102@gmail.com>
2020-09-25 17:54:08 +0900

    rename: make WikiLocation members more detailed
    add prefix 'wiki'

--- src/view/location_parser.ts
+++ src/view/location_parser.ts
@@ -1,5 +1,6 @@
-import {WikiType} from '../model/wiki_constant';
-type WikiLocation = {ns: string, type: WikiType, name: string};
+/* import {WikiType} from '../model/wiki_constant'; */
+type WikiType = 'Main' | 'Template' | 'File' | 'Special';
+type WikiLocation = {wikiNS: string, wikiType: WikiType, wikiName: string};
 
 const wikiTypeMap: Map<string, WikiType> = new Map([
     ['Main', 'Main'],
@@ -8,41 +9,45 @@ const wikiTypeMap: Map<string, WikiType> = new Map([
     ['Template', 'Template']
 ]);
 
-function parseWikiLocation(str: string): WikiLocation {
-    const loc: WikiLocation = {ns: 'Wiki', type: 'Main', name: ''};
-    const arr: string[] = str.split(':');
+function isWikiLink(href: string): boolean {
+    const PATTERN: RegExp = /https?:\/\/[\w\/:%#$&?()~.=+-]+/
+    return href.match(PATTERN) === null;
+}
+
+function parseWikiLocation(href: string): WikiLocation {
+    const loc: WikiLocation = {wikiNS: 'Wiki', wikiType: 'Main', wikiName: ''};
+    const arr: string[] = href.split(':');
     const v1: WikiType|undefined = wikiTypeMap.get(arr[0]);
     const v2: WikiType|undefined = wikiTypeMap.get(arr[1]);
     const len: number = arr.length;
     if (len === 1) {
-        loc.name = arr[0];
+        loc.wikiName = arr[0];
 
     } else if (len === 2) {
         if (v1 === undefined) {
-            loc.ns = arr[0];
+            loc.wikiNS = arr[0];
         } else {
-            loc.type = v1;
+            loc.wikiType = v1;
         }
-        loc.name = arr[1];
+        loc.wikiName = arr[1];
 
     } else if (len === 3) {
         if (v2 === undefined) {
-            throw new Error(`Invalid WikiType was found: ${loc.type}`);
+            throw new Error(`Invalid WikiType was found: ${loc.wikiType}`);
         }
-        loc.ns = arr[0];
-        loc.type = v2;
-        loc.name = arr[2];
+        loc.wikiNS = arr[0];
+        loc.wikiType = v2;
+        loc.wikiName = arr[2];
 
     } else {
-        throw new Error(`The number of colons must be 0 to 2: ${str}`);
+        throw new Error(`The number of colons must be 0 to 2: ${href}`);
     }
 
-    if (loc.ns.match(/^\s*$/)) {
-        throw new Error(`WikiNS must not be empty: ${str}`);
+    if (loc.wikiNS.match(/^\s*$/)) {
+        throw new Error(`WikiNS must not be empty: ${href}`);
     }
-    if (loc.name.match(/^\s*$/)) {
-        throw new Error(`WikiName must not be empty: ${str}`);
+    if (loc.wikiName.match(/^\s*$/)) {
+        throw new Error(`WikiName must not be empty: ${href}`);
     }
     return loc;
 }
-
