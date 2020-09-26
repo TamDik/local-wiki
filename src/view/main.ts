e823f3bdd05536e6995de8a01adb98f75ceb572c
daiki <dik.tkmr.1102@gmail.com>
2020-09-26 22:10:24 +0900

    add: MainViewView

--- src/view/main.ts
+++ src/view/main.ts
@@ -10,10 +10,30 @@ function dispatchWikiActionMain(controller: WikiController, wikiNS: string, wiki
     }
 }
 
+
 class MainViewView implements IContentView{
     public constructor(private $parent: JQuery, private wikiNS: string, private wikiName: string) {
     }
     public update(): void {
+        IpcAdapter.getPageText(this.wikiNS, this.wikiName)
+        .then(text => {
+            if (typeof(text) === 'string') {
+                const main: HTMLElement = wikimdToElement(text, 3);
+                this.$parent.append(main);
+            } else {
+                this.updateAsNotFoundView();
+            }
+        });
+    }
+
+    private updateAsNotFoundView(): void {
+        const lines: string[] = [];
+        const href: string = '?action=edit';
+        const msg1: string = 'There is currently no text in this page.';
+        lines.push('<div class="alert alert-warning" id="not-found-page-alert" role="alert">');
+        lines.push(`${msg1} You can <a href="${href}" class="internal-link alert-link">create this page</a>.`);
+        lines.push('</div>');
+        this.$parent.append(lines.join(''));
     }
 }
 
